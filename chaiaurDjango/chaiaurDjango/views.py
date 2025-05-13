b1b42d8b99beef46f0b614c728875a41becb98b0
shubham-151 <shubhamshukla@adroitfinancial.com>
2025-05-13 10:04:30 +0530

    All ok

--- chaiaurDjango/chaiaurDjango/views.py
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse 
+from django.shortcuts import render
 
 
 
 
 def home(request):
-    return HttpResponse("hello, world. You are at home page")
+    #return HttpResponse("hello, world. You are at home page")
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("Hello world. You are at about page")
