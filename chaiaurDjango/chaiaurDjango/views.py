decc8890438b38de00e41c720fed1634a427fec1
shubham-151 <shubhamshukla@adroitfinancial.com>
2025-05-29 11:40:07 +0530

    first commit

---
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse 
+from django.shortcuts import render
+
+
+
+
+def home(request):
+    #return HttpResponse("hello, world. You are at home page")
+    return render(request, 'index.html')
+
+def about(request):
+    return HttpResponse("Hello world. You are at about page")
+
+def contact(request):
+    return HttpResponse("Hello world. You are at contact page")
+
+
+
+
